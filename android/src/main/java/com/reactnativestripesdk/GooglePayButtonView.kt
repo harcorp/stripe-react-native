7b9ea39ba846f7c8461349c94b32ac9cd69129bf
David Rodriguez <david.rodriguez@kualty.com>
2022-11-15 03:27:34 -0500

    test

--- android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
+++ android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
@@ -7,19 +7,20 @@ import com.facebook.react.uimanager.ThemedReactContext
 class GooglePayButtonView(private val context: ThemedReactContext) : FrameLayout(context) {
   private var buttonType: String? = null
 
+  @Input
   fun initialize() {
-//    val type =
-//      when (buttonType) {
-//        "pay" -> R.layout.pay_with_googlepay_button
-//        "standard" -> R.layout.googlepay_button
-//        else -> R.layout.googlepay_button
-//      }
-//
-//    val button = LayoutInflater.from(context).inflate(
-//      type, null
-//    )
-//
-//    addView(button)
+    val type =
+      when (buttonType) {
+        "pay" -> R.layout.pay_with_googlepay_button
+        "standard" -> R.layout.googlepay_button
+        else -> R.layout.googlepay_button
+      }
+
+    val button = LayoutInflater.from(context).inflate(
+      type, null
+    )
+
+    addView(button)
   }
 
   fun setType(type: String) {
