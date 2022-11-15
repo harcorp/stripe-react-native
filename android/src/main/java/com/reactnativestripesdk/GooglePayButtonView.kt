4245d7f36ca5f07e2ad8bfb5fdbf9048cb168719
David Rodriguez <david.rodriguez@kualty.com>
2022-11-15 12:52:34 -0500

    chore: fix problem with google button

--- android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
+++ android/src/main/java/com/reactnativestripesdk/GooglePayButtonView.kt
@@ -8,18 +8,18 @@ class GooglePayButtonView(private val context: ThemedReactContext) : FrameLayout
   private var buttonType: String? = null
 
   fun initialize() {
-    val type =
-      when (buttonType) {
-        "pay" -> R.layout.pay_with_googlepay_button
-        "standard" -> R.layout.googlepay_button
-        else -> R.layout.googlepay_button
-      }
-
-    val button = LayoutInflater.from(context).inflate(
-      type, null
-    )
-
-    addView(button)
+//    val type =
+//      when (buttonType) {
+//        "pay" -> R.layout.pay_with_googlepay_button
+//        "standard" -> R.layout.googlepay_button
+//        else -> R.layout.googlepay_button
+//      }
+//
+//    val button = LayoutInflater.from(context).inflate(
+//      type, null
+//    )
+//
+//    addView(button)
   }
 
   fun setType(type: String) {
